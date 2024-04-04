f3f27b7aae25dd230551fc00c5a1d0ba95b1e6c3
Shyam Mahanta <smahanta118@gmail.com>
2024-04-04 21:36:07 +0530

    Project Setup on NextJS

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col text-2xl font-bold items-center justify-between p-24">
+      Hello World From NextJS
+    </main>
+  );
+}
