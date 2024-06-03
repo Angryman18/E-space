46e2363b72118258cb221e261db7e6383cb90c50
Shyam Mahanta <smahanta118@gmail.com>
2024-06-03 18:36:09 +0530

    updated test cases and updated jest config

--- __tests__/layout.test.tsx
+++ __tests__/layout.test.tsx
@@ -1,18 +1,41 @@
 import RootLayout from "@/app/(root)/layout";
-import { screen, render } from "@testing-library/react";
+import { TUser } from "@/types";
+import { screen, render, waitFor } from "@testing-library/react";
+
+jest.mock("next/navigation", () => ({
+  useRouter: jest.fn(() => ({ push: jest.fn() })),
+}));
+
+jest.mock("@/service/storeService", () => {
+  return {
+    __esModule: true,
+    fetchUserInfo: jest.fn(() => Promise.resolve({fullname: "Shyam Mahanta"})),
+  };
+});
 
 describe("Test for the Layout Component Rendering Properly", () => {
   beforeEach(() => {
     render(<RootLayout>Hello</RootLayout>);
   });
 
-  it("Should Render Out the Layout Children", () => {
-    const isChildRendering = screen.getByText("Hello");
+  it("Should Render Out the Layout Children", async () => {
+    const isChildRendering = await waitFor(() => screen.getByText("Hello"));
     expect(isChildRendering).toBeVisible();
   });
 
-  it("Should Render Out the body", () => {
-    const isbodyVisible = screen.getByTestId("layout_body");
-    expect(isbodyVisible).toBeVisible();
+  it("Should Render Out the body", async () => {
+    const searchBar = await waitFor(() => screen.getByTestId("search-bar"));
+    const recentSection = await waitFor(() => screen.getByTestId("recent-section"));
+    const sidebar = await waitFor(() => screen.getByTestId("sidebar"));
+    expect(searchBar).toBeVisible();
+    expect(recentSection).toBeVisible();
+    expect(sidebar).toBeVisible();
+  });
+
+  it("Should have shown the user's fullname properly", async () => {
+    const fullname = await waitFor(() => screen.getByTestId("user:fullname"));
+    const image = await waitFor(() => screen.getByTestId("user:image"));
+    expect(fullname).toHaveTextContent("Shyam Mahanta");
+    expect(image).toBeVisible();
   });
 });
--- __tests__/recent.test.tsx
+++ __tests__/recent.test.tsx
@@ -1,10 +1,23 @@
-import { screen, render } from "@testing-library/react";
+import { screen, render, waitFor, fireEvent } from "@testing-library/react";
 import Page from "@/app/(root)/page";
+import { MockThemeProvider } from "../__mock__/ContextMock";
 
 describe("Test Run", () => {
-  it("Should be be true", () => {
+  it("Should be be true", async () => {
     render(<Page />);
-    const text = screen.getByText("Toggle");
+    const text = await waitFor(() => screen.getByText("Toggle"));
+    expect(text).toBeVisible();
+  });
+  it("On Click the toggle the light/dark should work", async () => {
+    const setFn = jest.fn();
+    render(
+      <MockThemeProvider value={{ handleSetTheme: setFn, theme: "dark", isDark: false }}>
+        <Page />
+      </MockThemeProvider>
+    );
+    const text = await waitFor(() => screen.getByText("Toggle"));
+    fireEvent.click(text);
+    expect(setFn).toHaveBeenCalled();
     expect(text).toBeVisible();
   });
 });
